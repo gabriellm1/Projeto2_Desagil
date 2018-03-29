32c272af03122a55fc20cfc85e53f1d7defd9711
gabriellm1 <gabriellopesmonteiro41@gmail.com>
2018-03-29 20:54:47 -0300

    primeiras portas implementadas

---
+++ projeto2/src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/AndGate.java
@@ -0,0 +1,28 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.model;
+
+public class AndGate extends Gate {
+
+	private NandGate nandGate1;
+	private NandGate nandGate2;
+	
+	public AndGate() {
+		nandGate1 = new NandGate();
+		nandGate2 = new NandGate();
+		
+		nandGate1.connect(0,nandGate2);
+		nandGate1.connect(1,nandGate2);
+	}
+	
+	
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {
+		nandGate2.connect(pinIndex, emitter);
+	}
+
+	@Override
+	public boolean read() {
+		return nandGate1.read();
+	}
+}
+
+
---
+++ projeto2/src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/XorGate.java
@@ -0,0 +1,38 @@
+package br.pro.hashi.ensino.desagil.rafaelogic.model;
+
+public class XorGate extends Gate{
+
+	private NandGate nandGate1;
+	private NandGate nandGate2;
+	private NandGate nandGate3;
+	
+	public XorGate() {
+		
+		nandGate1 = new NandGate();
+		nandGate2 = new NandGate();
+		nandGate3 = new NandGate();
+		
+		nandGate1.connect(0,nandGate2);
+		nandGate1.connect(1,nandGate3);
+		
+	}
+	
+	
+	@Override
+	public void connect(int pinIndex, Emitter emitter) {
+		if(pinIndex == 0){
+			nandGate2.connect(0, emitter);
+			nandGate3.connect(1, emitter);
+		}
+		if(pinIndex == 1){
+			nandGate3.connect(0, emitter);
+			nandGate3.connect(1, emitter);
+		}
+
+	}
+
+	@Override
+	public boolean read() {
+		return nandGate1.read();
+	}
+}
