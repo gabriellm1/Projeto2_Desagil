a7da975b684b8850d6d65d61129b2db11bbc854c
Rafael Rosenzvaig <rafaelvr@al.insper.edu.br>
2018-03-29 19:22:36 -0300

    Xor testado

--- projeto2/src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/XorGate.java
+++ projeto2/src/main/java/br/pro/hashi/ensino/desagil/rafaelogic/model/XorGate.java
@@ -5,15 +5,19 @@ public class XorGate extends Gate{
 	private NandGate nandGate1;
 	private NandGate nandGate2;
 	private NandGate nandGate3;
+	private NandGate nandGate4;
 	
 	public XorGate() {
 		
 		nandGate1 = new NandGate();
 		nandGate2 = new NandGate();
 		nandGate3 = new NandGate();
+		nandGate4 = new NandGate();
 		
 		nandGate1.connect(0,nandGate2);
 		nandGate1.connect(1,nandGate3);
+		nandGate2.connect(1, nandGate4);
+		nandGate3.connect(0, nandGate4);
 		
 	}
 	
@@ -22,11 +26,11 @@ public class XorGate extends Gate{
 	public void connect(int pinIndex, Emitter emitter) {
 		if(pinIndex == 0){
 			nandGate2.connect(0, emitter);
-			nandGate3.connect(1, emitter);
+			nandGate4.connect(0, emitter);
 		}
 		if(pinIndex == 1){
-			nandGate3.connect(0, emitter);
 			nandGate3.connect(1, emitter);
+			nandGate4.connect(1, emitter);
 		}
 
 	}
